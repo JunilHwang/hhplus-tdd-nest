59ab6d4bf306babec3dd4b7e414b842d19b4f415
junilhwang <junil.h@kakao.com>
2025-07-05 15:01:18 +0900

    test: 테스트 객체 초기화

--- src/point/__tests__/point.controller.spec.ts
+++ src/point/__tests__/point.controller.spec.ts
@@ -1,3 +1,8 @@
+import { PointController } from '../point.controller';
+import { UserPointTable } from '../../database/userpoint.table';
+import { PointHistoryTable } from '../../database/pointhistory.table';
+import { Test, TestingModule } from '@nestjs/testing';
+
 /**
  * 포인트 관련 API 명세
  * - PATCH  `/point/{id}/charge` : 포인트를 충전한다.
@@ -7,6 +12,21 @@
  * - 잔고가 부족할 경우, 포인트 사용은 실패하여야 합니다.
  */
 describe('PointController > ', () => {
+  let controller: PointController;
+  let userPointTable: UserPointTable;
+  let pointHistoryTable: PointHistoryTable;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [PointController],
+      providers: [UserPointTable, PointHistoryTable],
+    }).compile();
+
+    controller = module.get<PointController>(PointController);
+    userPointTable = module.get<UserPointTable>(UserPointTable);
+    pointHistoryTable = module.get<PointHistoryTable>(PointHistoryTable);
+  });
+
   describe('포인트 충전', () => {});
 
   describe('포인트 사용', () => {});
