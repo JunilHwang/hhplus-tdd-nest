b69e10e4abedbf990a1840b4a70452d41551c9b6
junilhwang <junil.h@kakao.com>
2025-07-05 14:54:32 +0900

    test: point controller 테스트 파일 추가

---
+++ src/point/__tests__/point.controller.spec.ts
@@ -0,0 +1,19 @@
+/**
+ * 포인트 관련 API 명세
+ * - PATCH  `/point/{id}/charge` : 포인트를 충전한다.
+ * - PATCH `/point/{id}/use` : 포인트를 사용한다.
+ * - GET `/point/{id}` : 포인트를 조회한다.
+ * - GET `/point/{id}/histories` : 포인트 내역을 조회한다.
+ * - 잔고가 부족할 경우, 포인트 사용은 실패하여야 합니다.
+ */
+describe('PointController > ', () => {
+  describe('포인트 충전', () => {});
+
+  describe('포인트 사용', () => {});
+
+  describe('포인트 조회', () => {});
+
+  describe('포인트 내역 조회', () => {});
+
+  describe('잔고가 관리', () => {});
+});
