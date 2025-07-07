583b0c6ee0c1f5bf9641115825a416243dedb49d
junilhwang <junil.h@kakao.com>
2025-07-07 22:34:15 +0900

    feat: 포인트 조회에 대한 기능 추가

--- src/point/point.service.ts
+++ src/point/point.service.ts
@@ -1,23 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { UserPointTable } from '../database/userpoint.table';
-import { PointHistoryTable } from '../database/pointhistory.table';
 import { PointException } from './point.error';
 
 @Injectable()
 export class PointService {
-  constructor(
-    // Inject the necessary database tables or repositories here
-    private readonly userPointTable: UserPointTable,
-    private readonly pointHistoryTable: PointHistoryTable,
-  ) {}
+  constructor(private readonly userPointTable: UserPointTable) {}
 
   async chargePoint(userId: number, amount: number) {
     if (amount <= 0 || !Number.isInteger(amount)) {
       throw new PointException('충전 금액은 양의 정수여야 합니다.');
     }
 
-    console.log(userId, amount);
-
     return await this.userPointTable.insertOrUpdate(userId, amount);
+  }
+
+  async getPoint(userId: number) {
+    return await this.userPointTable.selectById(userId);
   }
 }
